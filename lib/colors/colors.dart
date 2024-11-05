52c951585311ee05919759d4168c046187046b5e
exam <exam@Aptechnk1.edu>
2024-11-05 18:55:43 +0500

    taha

---
+++ lib/colors/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class CustomColors {
+  static Color bt_bg = Color(0xffFCC873);
+  static Color text_bg = Color(0xff0F141D);
+   static Color text_logo = Color(0xffFFFFFF);
+   
+
+}
